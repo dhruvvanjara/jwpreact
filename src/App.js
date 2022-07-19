49ad3d56b66efbb8327132c460d7cbb4a2daa248
devjasim <jasimfbd@gmail.com>
2022-07-19 22:46:56 +0600

    slider-imple

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Layout from "./components/layout/layout";
 import Replay from "./components/replay/index";
+import Slider from './components/Slider/index'
+
 function App() {
   return (
     <div className="App">
       <Layout />
+      <Slider/>
       <Replay />
     </div>
   );
